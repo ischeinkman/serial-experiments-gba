b3d5f01986ca939886bbd42081440c8c1a406500
ischeinkman <scheinkman.ilan@gmail.com>
2024-06-14 20:59:28 -0400

    Cleanups to the GPIO module

--- src/serial/generalpurpose.rs
+++ src/serial/generalpurpose.rs
@@ -1,4 +1,12 @@
+//! The GBA allows the serial port to be used as a 4-pin GPIO parallel port,
+//! which each pin being able to be used as either an input or an output.
+
 use core::marker::PhantomData;
+
+use agb::{
+    external::critical_section::CriticalSection,
+    interrupt::{add_interrupt_handler, Interrupt, InterruptHandler},
+};
 
 use crate::utils::{read_bit_u8, write_bit_u8};
 
@@ -26,13 +34,11 @@ impl GpioDirection {
     pub const fn is_input(&self) -> bool {
         !self.is_output()
     }
-    const fn as_u16(&self) -> u16 {
-        *self as u8 as u16
-    }
 }
 
 pub struct GeneralPurpose<'a> {
     _handle: PhantomData<&'a mut Serial>,
+    interrupt_handle: Option<InterruptHandler>,
 }
 
 impl<'a> GeneralPurpose<'a> {
@@ -40,17 +46,19 @@ impl<'a> GeneralPurpose<'a> {
         RcntWrapper::get().set_mode(SerialMode::Gpio);
         Self {
             _handle: PhantomData,
+            interrupt_handle: None,
         }
     }
     pub fn from_handle<'b: 'a>(_handle: &'a mut PhantomData<&'b mut Serial>) -> Self {
         Self {
             _handle: PhantomData,
+            interrupt_handle: None,
         }
     }
     pub fn gpio_config(&self) -> GpioConfig {
         GpioConfig::from_rcnt(RcntWrapper::new().read())
     }
-    pub fn set_gpio_config(&self, cfg: GpioConfig) {
+    pub fn set_gpio_config(&mut self, cfg: GpioConfig) {
         let rcnt = RcntWrapper::get();
         let old = rcnt.read();
         let masked = old & !GpioConfig::MASK;
@@ -60,47 +68,83 @@ impl<'a> GeneralPurpose<'a> {
     pub fn interupt_enabled(&self) -> bool {
         RcntWrapper::get().si_interrupt_enabled()
     }
-    pub fn set_interrupt(&self, interupt: bool) {
+    pub fn enable_interrupt(&mut self, interupt: bool) {
         RcntWrapper::get().enable_si_interrupt(interupt)
     }
+    /// Adds a callback that will be called whenever the SI line is set to HIGH.
+    ///
+    /// # Safety
+    /// The callback `cb` **must not** allocate.
+    pub unsafe fn set_interrupt(&mut self, cb: impl Fn(CriticalSection) + Send + Sync + 'static) {
+        self.interrupt_handle = Some(add_interrupt_handler(Interrupt::Serial, cb));
+    }
+    /// Gets the current state of the GPIO pins.
     pub fn pins(&self) -> PinState {
         PinState::from_rcnt(RcntWrapper::get().read())
     }
-    pub fn write_pins(&self, state: PinState) {
+    /// Writes the state of all 4 GPIO pins at once.
+    pub fn write_pins(&mut self, state: PinState) {
         let old = RcntWrapper::get().read();
         let new = (old & !PinState::MASK) | state.into_rcnt();
         RcntWrapper::get().write(new)
     }
-    pub fn write_pin(&self, pin: Pin, high: bool) {
+    /// Sets a pin to either HIGH or LOW.
+    pub fn write_pin(&mut self, pin: Pin, high: bool) {
         RcntWrapper::get().write_bit(pin as u8, high)
     }
+
+    /// Gets the state of the pin.
     pub fn read_pin(&self, pin: Pin) -> bool {
         RcntWrapper::get().read_bit(pin as u8)
+    }
+
+    pub fn state(&self) -> GpioState {
+        GpioState::from_rcnt(RcntWrapper::get().read())
     }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default)]
 pub struct GpioConfig {
-    pub sc: GpioDirection,
-    pub sd: GpioDirection,
-    pub si: GpioDirection,
-    pub so: GpioDirection,
+    value: u8,
 }
 
 impl GpioConfig {
     const MASK: u16 = 0xFu16 << 4;
+    pub const fn sc(&self) -> GpioDirection {
+        GpioDirection::from_is_output(read_bit_u8(self.value, 4))
+    }
+    pub const fn with_sc(self, dir: GpioDirection) -> Self {
+        let value = write_bit_u8(self.value, 4, dir.is_output());
+        Self { value }
+    }
+    pub const fn sd(&self) -> GpioDirection {
+        GpioDirection::from_is_output(read_bit_u8(self.value, 5))
+    }
+    pub const fn with_sd(self, dir: GpioDirection) -> Self {
+        let value = write_bit_u8(self.value, 5, dir.is_output());
+        Self { value }
+    }
+    pub const fn si(&self) -> GpioDirection {
+        GpioDirection::from_is_output(read_bit_u8(self.value, 6))
+    }
+    pub const fn with_si(self, dir: GpioDirection) -> Self {
+        let value = write_bit_u8(self.value, 6, dir.is_output());
+        Self { value }
+    }
+    pub const fn so(&self) -> GpioDirection {
+        GpioDirection::from_is_output(read_bit_u8(self.value, 7))
+    }
+    pub const fn with_so(self, dir: GpioDirection) -> Self {
+        let value = write_bit_u8(self.value, 7, dir.is_output());
+        Self { value }
+    }
     const fn from_rcnt(value: u16) -> Self {
-        let sc = GpioDirection::from_is_output((value & (1 << 4)) != 0);
-        let sd = GpioDirection::from_is_output((value & (1 << 5)) != 0);
-        let si = GpioDirection::from_is_output((value & (1 << 6)) != 0);
-        let so = GpioDirection::from_is_output((value & (1 << 7)) != 0);
-        Self { sc, sd, si, so }
+        Self {
+            value: (value & Self::MASK) as u8,
+        }
     }
     const fn into_rcnt(self) -> u16 {
-        (self.sc.as_u16() << 4)
-            | (self.sd.as_u16() << 5)
-            | (self.si.as_u16() << 6)
-            | (self.so.as_u16() << 7)
+        self.value as u16
     }
 }
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default)]
@@ -160,18 +204,22 @@ impl PinState {
 }
 
 pub struct GpioState {
-    pub pins: PinState,
-    pub config: GpioConfig,
+    value: u8,
 }
 
 impl GpioState {
+    pub const fn pins(&self) -> PinState {
+        PinState::from_rcnt(self.value as u16)
+    }
+    pub const fn config(&self) -> GpioConfig {
+        GpioConfig::from_rcnt(self.value as u16)
+    }
     const fn from_rcnt(rcnt: u16) -> Self {
         Self {
-            pins: PinState::from_rcnt(rcnt),
-            config: GpioConfig::from_rcnt(rcnt),
+            value: (rcnt & 0xFF) as u8,
         }
     }
     const fn into_rcnt(self) -> u16 {
-        self.pins.into_rcnt() | self.config.into_rcnt()
+        self.value as u16
     }
 }
